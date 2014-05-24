d7ac6a59f793b5bb10da37b9c95243028f3ffa1f
podkolzzzin <1996*zorro>
2014-05-24 20:00:32 +0300

    More

--- PZ13.1/Un_main.h
+++ PZ13.1/Un_main.h
@@ -11,6 +11,15 @@
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+        TEdit *Max_edit;
+        TGroupBox *GroupBox1;
+        TCheckBox *CheckBox1;
+        TCheckBox *CheckBox2;
+        TLabel *Lbl_output;
+        TButton *Btn_set;
+        void __fastcall CheckBox1Click(TObject *Sender);
+        void __fastcall CheckBox2Click(TObject *Sender);
+        void __fastcall Btn_setClick(TObject *Sender);
 private:	// User declarations
         void Resize(TControl*);
 public:		// User declarations
